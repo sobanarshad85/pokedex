3928a2b454183b8475d97099c5a12be076c35fc4
soban1234 <soban.arshad@doorhub.io>
2023-02-08 18:28:54 +0500

    pagination implemented.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,47 +1,62 @@
 import React, { useEffect, useMemo } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { addPokemons } from "../redux/reducers";
+import {
+  addPokemons,
+  setOffset,
+  setLoading,
+  setPageLimit,
+} from "../redux/reducers";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import { useRouter } from "next/router";
 
 type Row = { row: { col1: string } };
 type Pokemon = { name: string; url: string };
+type State = {
+  pokemons: Pokemon[];
+  pageOffset: number;
+  loading: boolean;
+  pageLimit: number;
+  totalRecords: number;
+};
 
 const Page = () => {
-  const pokemons = useSelector(
-    (state: { pokemons: Pokemon[] }) => state.pokemons
-  );
+  const pokemons = useSelector((state: State) => state.pokemons);
+  const pageOffset = useSelector((state: State) => state.pageOffset);
+  const loading = useSelector((state: State) => state.loading);
+  const pageLimit = useSelector((state: State) => state.pageLimit);
+  const totalRecords = useSelector((state: State) => state.totalRecords);
   const dispatch = useDispatch();
   const router = useRouter();
 
   useEffect(() => {
     fetchPokemon();
-  }, []);
+  }, [pageOffset, pageLimit]);
 
   const fetchPokemon = async () => {
-    fetch("https://pokeapi.co/api/v2/pokemon")
+    dispatch(setLoading(true));
+    fetch(
+      `https://pokeapi.co/api/v2/pokemon?offset=${
+        pageOffset * pageLimit
+      }&limit=${pageLimit}`
+    )
       .then((res) => {
-        res.json().then((data) => dispatch(addPokemons(data.results)));
+        res.json().then((data) => dispatch(addPokemons(data)));
       })
-      .catch()
-      .finally();
+      .catch(() => dispatch(setLoading(false)));
   };
 
   const rows = useMemo(() => {
-    return pokemons.length >= 1
-      ? pokemons.map((pokemon: Pokemon, index: number) => {
-          return {
-            id: index + 1,
-            col1: pokemon.name,
-          };
-        })
-      : [];
+    return pokemons.map((pokemon: Pokemon, index: number) => {
+      return {
+        id: index + 1 + pageLimit * pageOffset,
+        col1: pokemon.name,
+      };
+    });
   }, [pokemons]);
 
   const columns: GridColDef[] = [
     { field: "id", headerName: "ID" },
-    { field: "col1", headerName: "Column 1", width: 150 },
-    { field: "col2", headerName: "Column 2", width: 150 },
+    { field: "col1", headerName: "Name1", width: 250 },
   ];
 
   const handleRowClick = (row: Row) => {
@@ -52,13 +67,19 @@ const Page = () => {
 
   return (
     <div>
-      <div style={{ height: 300, width: "100%" }}>
+      <div style={{ display:'flex',flex:1, height: '70vh', width: "100%" }}>
         <DataGrid
-          pageSize={10}
-          // rowsPerPageOptions={[100]}
-          // page={0}
+          loading={loading}
+          rowCount={totalRecords ?? 100}
+          pageSize={pageLimit}
+          rowsPerPageOptions={[5, 10, 100]}
+          onPageChange={(newPage) => dispatch(setOffset(newPage))}
+          onPageSizeChange={(limit) => dispatch(setPageLimit(limit))}
+          page={pageOffset}
           rows={rows}
           columns={columns}
+          // autoHeight
+          paginationMode="server"
           onRowClick={handleRowClick}
         />
       </div>
--- redux/reducers.js
+++ redux/reducers.js
@@ -2,12 +2,38 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const pokeSlice = createSlice({
   name: "pokemon",
-  initialState: { pokemons: [], loading: true, error: null },
+  initialState: {
+    pokemons: [],
+    loading: true,
+    error: null,
+    pageOffset: 0,
+    pageLimit: 10,
+    totalRecords: 0,
+  },
   reducers: {
+    setOffset: (state, action) => {
+      return {
+        ...state,
+        pageOffset: action.payload,
+      };
+    },
+    setPageLimit: (state, action) => {
+      return {
+        ...state,
+        pageLimit: action.payload,
+      };
+    },
+    setLoading: (state, action) => {
+      return {
+        ...state,
+        loading: action.payload,
+      };
+    },
     addPokemons: (state, action) => {
       return {
         ...state,
-        pokemons: action.payload,
+        pokemons: action.payload.results,
+        totalRecords: action.payload.count,
         loading: false,
         error: null,
       };
@@ -15,6 +41,7 @@ const pokeSlice = createSlice({
   },
 });
 
-export const { addPokemons } = pokeSlice.actions;
+export const { addPokemons, setOffset, setLoading, setPageLimit } =
+  pokeSlice.actions;
 
 export default pokeSlice.reducer;
